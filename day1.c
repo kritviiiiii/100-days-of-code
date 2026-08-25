3c64c2afaf230013c13a0313653db4d1d881217b
kritviiiiii <kritvi1308@gmail.com>
2026-08-25 16:48:11 +0530

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,13 @@
+//DAY1 
+//Write a program to input two numbers and display their sum.
+#include <stdio.h>
+ int main () 
+ {
+ int a,b;
+ int c;
+ printf ("Enter Two Numbers\n");
+ scanf("%d %d",&a,&b);
+ c=a+b;
+ printf("%d",c);
+ return 0;
+ }
